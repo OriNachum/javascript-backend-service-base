b5a297fd085f5e22a72bef027b72af9b8a38375a
Ori Nachum <ori.nachum@gmail.com>
2023-04-15 17:00:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = require('express')();
+const { v4 } = require('uuid');
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/test', (req, res) => {
-  res.send('works!');
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
